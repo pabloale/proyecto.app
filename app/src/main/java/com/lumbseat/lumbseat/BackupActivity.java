ed9bdd3528d1a73931d3ee15f94d5ccee4d0c7ea
Matias Cutri <42496592+maticutri@users.noreply.github.com>
2018-11-23 17:58:24 -0300

    Restore Funcionando
    APP FINALIZADA

--- app/src/main/java/com/lumbseat/lumbseat/BackupActivity.java
+++ app/src/main/java/com/lumbseat/lumbseat/BackupActivity.java
@@ -44,10 +44,13 @@ import com.lumbseat.lumbseat.dataBase.SQLiteConnectionHelper;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -63,7 +66,7 @@ public class BackupActivity extends Activity {
     private SharedPreferences myPreferences;
     private TextView tvDiaBk;
 
-    private String driveID = "";
+    public static DriveId driveID;
 
     private DataBufferAdapter<Metadata> mResultsAdapter;
 
@@ -101,15 +104,10 @@ public class BackupActivity extends Activity {
 
         MainActivity.contextoActual = this;
 
-        ListView mListView = findViewById(R.id.listViewResults);
-        mResultsAdapter = new ResultAdapter(this, 1);
-        mListView.setAdapter(mResultsAdapter);
-
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.navigation_backup);
         navigation.setItemTextColor(ColorStateList.valueOf(Color.WHITE));
-
 
         myPreferences = PreferenceManager.getDefaultSharedPreferences(BackupActivity.this);
         tvDiaBk = (TextView) findViewById(R.id.tvDiaBk);
@@ -200,13 +198,16 @@ public class BackupActivity extends Activity {
             Log.w(TAG, "Unable to write file contents.", e);
         }
 
+
+        Date fechaHoy = new Date();
+        String fechaHoyFormateada = new SimpleDateFormat("dd/MM/yyyyHH:mm:ss").format(fechaHoy);
         // Create the initial metadata - MIME type and title.
         // Note that the user will be able to change the title later.
         //String mimeType = MimeTypeMap.getSingleton().getExtensionFromMimeType(MIME_TYPE);
         MetadataChangeSet metadataChangeSet =
                 new MetadataChangeSet.Builder()
                         .setMimeType(MIME_TYPE)
-                        .setTitle("bd_datos")
+                        .setTitle("bd_datos"+fechaHoyFormateada)
                         .build();
 
         // Set up options to configure and display the create file activity.
@@ -223,7 +224,6 @@ public class BackupActivity extends Activity {
                             @Override
                             public Void then(@NonNull Task<IntentSender> task) throws Exception {
                                 startIntentSenderForResult(task.getResult(), REQUEST_DB_UPLOADER, null, 0, 0, 0);
-                                driveID = createFileActivityOptions.EXTRA_RESPONSE_DRIVE_ID;
                                 return null;
                             }
                         });
@@ -248,6 +248,7 @@ public class BackupActivity extends Activity {
                 Log.i(TAG, "Upload file request");
                 // Called after a file is saved to Drive.
                 if (resultCode == RESULT_OK) {
+                    driveID = data.getParcelableExtra(CreateFileActivityOptions.EXTRA_RESPONSE_DRIVE_ID);
 
                     Date cDate = new Date();
                     String fechaHoy = new SimpleDateFormat("dd/MM/yyyy").format(cDate);
@@ -258,8 +259,7 @@ public class BackupActivity extends Activity {
                     tvDiaBk.setText(fechaHoy);
 
                     Log.i(TAG, "DB file successfully saved.");
-                    Toast.makeText(BackupActivity.this,"Sus datos han sido resguardados",Toast.LENGTH_SHORT).show();
-                    Toast.makeText(BackupActivity.this, "DRIVE ID: "+ driveID, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(BackupActivity.this,"Sus datos han sido resguardados correctamente!",Toast.LENGTH_SHORT).show();
                 }else if(resultCode == RESULT_CANCELED){
                     Toast.makeText(BackupActivity.this,"Ha cancelado el Backup de sus datos",Toast.LENGTH_SHORT).show();
                 }
@@ -268,111 +268,26 @@ public class BackupActivity extends Activity {
     }
 
     private void getDbFromDrive() {
-        //SQLiteConnectionHelper sqlCnnhelper = new SQLiteConnectionHelper();
+        Task<DriveContents> openFile = LoginActivity.mDriveResourceClient.openFile(driveID.asDriveFile(),DriveFile.MODE_READ_ONLY);
 
-
-        Query query = new Query.Builder()
-                .addFilter(Filters.eq(SearchableField.TITLE, "bd_datos"))
-                .build();
-
-/*
-        MetadataBuffer buffer = null;
-        try {
-            buffer = Drive.DriveApi.getAppFolder(ConfigurationActivity.mGoogleApiClient)
-                    .queryChildren(ConfigurationActivity.mGoogleApiClient, query).await().getMetadataBuffer();
-
-            if (buffer != null && buffer.getCount() > 0) {
-                buffer.get(0).getDriveId();
-                //sqlCnnhelper.onUpgrade(,1,2);
+        openFile.continueWithTask(task -> {
+            DriveContents contents = task.getResult();
+            try (BufferedInputStream reader = new BufferedInputStream(contents.getInputStream())){
+                int n;
+                byte[] buffer = new byte[1024];
+                BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(DATA_DIRECTORY_DATABASE));
+                while ((n = reader.read(buffer)) > 0) {
+                    bos.write(buffer, 0, n);
+                }
+                bos.close();
             }
+            Toast.makeText(BackupActivity.this,"Sus datos han sido restaurados correctamente!",Toast.LENGTH_SHORT).show();
 
-        } finally {
-            if (buffer != null) {
-                buffer.close();
-            }
-        }*/
-
-        // [START drive_android_query_files]
-        Task<MetadataBuffer> queryTask = LoginActivity.mDriveResourceClient.query(query)
-                .addOnSuccessListener(this, metadataBuffer -> mResultsAdapter.append(metadataBuffer))
-                .addOnFailureListener(this, e -> {
-                    Log.e(TAG,"asd" + e);
-                    finish();
-                });
+            Task<Void> discardTask = LoginActivity.mDriveResourceClient.discardContents(contents);
+            return discardTask;
+        }).addOnFailureListener(e -> {
+            Log.e(TAG, "Unable to read contents", e);
+            finish();
+        });
     }
-
-    public class ResultAdapter extends DataBufferAdapter<Metadata> {
-        public ResultAdapter(Context context, int i) {
-            super(context, i);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent){
-            if(convertView == null){
-                convertView = View.inflate(getContext(),
-                        android.R.layout.simple_list_item_1, null);
-            }
-            Metadata metadata = getItem(position);
-            TextView titleTextView = (TextView) convertView.findViewById(android.R.id.text1);
-            titleTextView.setText(metadata.getTitle());
-            return convertView;
-        }
-    }
-
 }
-
-
-
-/*file.open(mGoogleApiClient, DriveFile.MODE_READ_ONLY, null)
-                .setResultCallback(new ResultCallback<DriveApi.DriveContentsResult>() {
-                    @Override
-                    public void onResult(DriveApi.DriveContentsResult result) {
-                        if (!result.getStatus().isSuccess()) {
-                            showErrorDialog();
-                            return;
-                        }
-
-                        // DriveContents object contains pointers
-                        // to the actual byte stream
-                        DriveContents contents = result.getDriveContents();
-                        InputStream input = contents.getInputStream();
-
-                        try {
-                            File file = new File(realm.getPath());
-                            OutputStream output = new FileOutputStream(file);
-                            try {
-                                try {
-                                    byte[] buffer = new byte[4 * 1024]; // or other buffer size
-                                    int read;
-
-                                    while ((read = input.read(buffer)) != -1) {
-                                        output.write(buffer, 0, read);
-                                    }
-                                    output.flush();
-                                } finally {
-                                    output.close();
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-                        } finally {
-                            try {
-                                input.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                        Toast.makeText(getApplicationContext(), R.string.activity_backup_drive_message_restart, Toast.LENGTH_LONG).show();
-
-                        // Reboot app
-                        Intent mStartActivity = new Intent(getApplicationContext(), MainActivity.class);
-                        int mPendingIntentId = 123456;
-                        PendingIntent mPendingIntent = PendingIntent.getActivity(getApplicationContext(), mPendingIntentId, mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT);
-                        AlarmManager mgr = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-                        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
-                        System.exit(0);
-                    }
-                });*/
